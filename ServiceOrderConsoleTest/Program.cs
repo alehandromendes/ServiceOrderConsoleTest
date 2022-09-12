2aa8a1318786738da55f872cb761998e5a1fe5d1
aleha <aleha@DESKTOP-LLLR2GS>
2022-09-12 20:59:21 -0300

    Adicionar arquivos de projeto.

---
+++ ServiceOrderConsoleTest/Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Newtonsoft.Json;
+using ServiceOrderConsoleTest.Models;
+using System.Net.Http.Headers;
+
+
+namespace ServiceOrderConsoleTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Document doc = new Document(PageSize.A4);
+            doc.SetMargins(40, 40, 40, 80);
+            //Setando o caminho
+            string caminho = @"D:\[DEV]\ConsoleApps\ServiceOrders\ServiceOrderConsoleTest\MakingPDF\pdf_files\" + "arquivo2.pdf";
+
+            //Criando o arquivo
+            PdfWriter writer= PdfWriter.GetInstance(doc, new FileStream(caminho, FileMode.Create));
+
+            // Abrindo o arquivo
+            doc.Open();
+
+            //Configurações de titulo
+            Paragraph titulo = new Paragraph();
+            titulo.Font = new Font(Font.FontFamily.COURIER, 40);
+            titulo.Alignment = Element.ALIGN_CENTER;
+            titulo.Add("Teste\n\n"); //Titulo do arquivo
+
+            //Adicionando ao documento
+            doc.Add(titulo);
+
+            Paragraph paragrafo = new Paragraph("", new Font(Font.NORMAL, 12)); //Segundo paragrafo (texto comum)
+            string conteudo = "Testando arquivo\n\n";
+            paragrafo.Add(conteudo); //adicionando ao paragrafo
+            doc.Add(paragrafo); //adicionando ao documento
+
+            //Criando tabela de dados
+            PdfPTable table = new PdfPTable(3);
+            table.AddCell("Linha 1, Coluna 1");
+            table.AddCell("Linha 1, Coluna 2");
+            table.AddCell("Linha 1, Coluna 3");
+
+            table.AddCell("Linha 2, Coluna 1");
+            table.AddCell("Linha 2, Coluna 2");
+            table.AddCell("Linha 2, Coluna 3");
+
+            table.AddCell("Linha 3, Coluna 1");
+            table.AddCell("Linha 3, Coluna 2");
+            table.AddCell("Linha 3, Coluna 3");
+
+            doc.Add(table);
+
+            doc.Close();
+
+            System.Diagnostics.Process.Start(caminho);
+        }   
+
+    }
+}
---
+++ ServiceOrderConsoleTest/ServiceOrders.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceOrderConsoleTest.Models
+{
+    public class ServiceOrders
+    {
+        [Key]
+        [JsonProperty("totalExecutingActivities")]
+        public int TotalExecutingActivities { get; set; }
+        [JsonProperty("totalNewActivities")]
+        public int TotalNewActivities { get; set; }
+        [JsonProperty("totalPausedActivities")]
+        public int TotalPausedActivities { get; set; }
+        [JsonProperty("totalCancelledActivities")]
+        public int TotalCancelledActivities { get; set; }
+        [JsonProperty("totalClosedActivities")]
+        public int TotalClosedActivities { get; set; }
+        [JsonProperty("newSo")]
+        public bool NewSo { get; set; }
+        [JsonProperty("beginDate")]
+        public DateTime beginDate { get; set; }
+        [JsonProperty("defect")]
+        public string Defect { get; set; }
+        [JsonProperty("accountCode")]
+        public string AccountCode { get; set; }
+        [JsonProperty("personOpen")]
+        public string PersonOpen { get; set; }
+        [JsonProperty("account")]
+        public string Account { get; set; }
+        [JsonProperty("accountId")]
+        public int AccountId { get; set; }
+        [JsonProperty("sequentialId")]
+        public int SequentialId { get; set; }
+        [JsonProperty("status")]
+        public int Status { get; set; }
+        [JsonProperty("imported")]
+        public bool Imported { get; set; }
+        [JsonProperty("comments")]
+        public List<comments> Comments { get; set; }
+        [JsonProperty("accountOffset")]
+        public int AccountOffset { get; set; }
+        [JsonProperty("executingActivities")]
+        public bool ExecutingActivities { get; set; }
+        [JsonProperty("endDate")]
+        public DateTime EndDate { get; set; }
+        [JsonProperty("personOpenId")]
+        public int PersonOpenId { get; set; }
+        [JsonProperty("id")]
+        public int Id { get; set; }
+    }
+    public class comments
+    {
+        [JsonProperty("date")]
+        public DateTime Date { get; set; }
+        [JsonProperty("text")]
+        public string Text { get; set; }
+        [JsonProperty("id")]
+        public int Id{ get; set; }
+    }
+}
